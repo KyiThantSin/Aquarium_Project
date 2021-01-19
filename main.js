66009792e0d6a73a3ffefae72cae6d0df519ab3a
KyiThantSin <kyithantsin@mtu.edu.mm>
2021-01-19 22:24:15 +0630

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function checkNo(){
+    let d=new Date()
+    alert(`Thank You..
+    Confirmation mail will be sent to you in a minute.
+    (Payment method and details info about tickets included).
+    
+  Date:${d.getDay()}/${d.getMonth()}/${d.getFullYear()}
+                                        Waikiki's Aquarium`)
+      
+        
+}
+function seeMap(){
+   document.getElementById("map").style.display="block";
+}
